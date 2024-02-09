ec7ca6025ea62b285f0839180d7862ec83a79b67
lukejwamble <wambleson3@gmail.com>
2024-02-09 12:19:21 -0600

    push 1
    added function and ran tests in terminal, all passed, running now to git

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(word1, word2):
+    if word1 == " ": return False
+    else:
+        word1 = word1.lower().replace(" ", "")
+        word2 = word2.lower().replace(" ", "")
+        
+        splitword1 = []
+        for i in word1:
+            splitword1.append(i)
+
+        for i in splitword1:
+            if i in word2:
+                continue
+            else: return False
+        return True
+
 
 if __name__ == '__main__':
     #REMOVE PASS YOUR CODE GOES HERE
-    pass
-    
+    word1 = input()
+    word2 = input()
+
+    anagram(word1, word2)
